3bdb1d2fbb3126f2d75b63f260856c2429ee569f
kTorp <kristorp@amd.com>
2026-08-20 00:09:20 -0500

    Add comms & comp reorder flags

--- .ci/integrations/vllm_omni/online_runner.py
+++ .ci/integrations/vllm_omni/online_runner.py
@@ -83,6 +83,7 @@ def parse_args() -> argparse.Namespace:
     )
     server.add_argument(
         "--diffusion_compile_reorder_comm_overlap",
+        "--diffusion-compile-reorder-comm-overlap",
         action=argparse.BooleanOptionalAction,
         default=False,
         help="Enable Inductor compute/communication overlap reordering.",
